7f9edad9ff13934078b1c5a4d167ca896165a801
heldgrind <corentin.biosca@gmail.com>
2023-04-30 20:36:01 +0200

    presque la fin

--- TP_03/scripts/run_nornir.py
+++ TP_03/scripts/run_nornir.py
@@ -1,4 +1,30 @@
 from nornir import InitNornir
+from nornir.core.task import Task, Result
+from nornir_utils.plugins.functions import print_result
+from nornir_napalm.plugins.tasks import napalm_get,napalm_configure, napalm_cli
+from nornir_netmiko.tasks import netmiko_send_config,netmiko_send_command, netmiko_save_config, netmiko_commit
+
+
+def hello_world(task: Task) -> Result:
+    return Result(
+    host=task.host,
+    result=f"{task.host.name} says hello world!"
+    )
+def save (task: Task) -> Result:
+    r = task.run(napalm_cli, commands=["wr"])
+    return Result(
+    host=task.host,
+    result=r.result
+    )
+
+def state (task: Task) -> Result:
+    r = task.run(netmiko_send_command, command_string="show ip int brief")
+    return Result(
+    host=task.host,
+    result=r.result
+    )
+
+
 
 def question_13(nr):
     print(nr.__dict__)
@@ -28,63 +54,121 @@ def question_16(nr):
     pass
 
 def question_17(nr):
+    premier = nr.inventory.hosts['R1-CPE-BAT-B']
+    print((premier.keys()))
     pass
 
 def question_18(nr):
+    premier = nr.inventory.hosts['R1-CPE-BAT-B']
+    print(premier.__getitem__("room"))
     pass
 
 def question_19(nr):
+    premier = nr.inventory.hosts['R1-CPE-BAT-B']
+    print(nr.inventory.groups)
     pass
 
 def question_20(nr):
+    premier = nr.inventory.hosts['R1-CPE-BAT-B']
+    groupes = nr.inventory.hosts.get('R1-CPE-BAT-A').groups
+    print(groupes)
     pass
 
 def question_21(nr):
+    premier = nr.inventory.hosts['R1-CPE-BAT-B']
+    groupeskey = nr.inventory.hosts.get('R1-CPE-BAT-A').groups[0].keys()
+    print(groupeskey)
     pass
 
 def question_22(nr):
+    premier = nr.inventory.hosts['R1-CPE-BAT-A']
+    groupeskey = nr.inventory.hosts.get('R1-CPE-BAT-A').groups[0].get('vendor')
+    print(groupeskey)
     pass
 
 def question_23(nr):
+    nr = InitNornir(config_file="inventory/config.yaml")
+    hostname = nr.inventory.hosts
+    for host in hostname:
+        ip = nr.inventory.hosts.get(host).hostname
+        print(ip) 
     pass
 
 def question_24(nr):
+    print(nr.filter(device_type="router").inventory.hosts.keys())
     pass
 
 def question_25(nr):
+    print(nr.filter(device_type="router_switch").inventory.hosts.keys())
     pass
 
 def question_26(nr):
+    result = nr.run(task=hello_world)
+    print(result)
     pass
 
 def question_27(nr):
+    result = nr.run(task=hello_world)
+    print(type(result))
     pass
 
 def question_29(nr):
+    result = nr.run(task=hello_world)
+    print_result(result)
     pass
 
 def question_30(nr):
+    test_group = nr.filter(device_type="router_switch")
+    result = test_group.run(task=hello_world)
+    print_result(result)
     pass
 
 def question_32(nr):
+    test_group = nr.filter(device_type="router")
+    result = test_group.run(task=napalm_get, getters=["arp_table"])
     pass
  
 def question_33(nr):
+    test_group = nr.filter(device_type="router_switch")
+    result = test_group.run(task=napalm_get, getters=["arp_table"])
+    print_result(result)
     pass
 
 def question_34(nr):
+    Router1_A = nr.filter(device_name="R1-CPE-BAT-A")
+    result = Router1_A.run(task=napalm_configure, configuration="interface Loopback 1 \n ip address 1.1.1.1 255.255.255.255")
+    print_result(result)
+    Router2_A = nr.filter(device_name="R2-CPE-BAT-A")
+    result = Router2_A.run(task=napalm_configure, configuration="interface Loopback 1 \n ip address 2.2.2.2 255.255.255.255")
+    print_result(result)
     pass
 
 def question_35(nr):
+    result = nr.run(task=save)
+    print_result(result)
     pass
 
 def question_36(nr):
+    result = nr.run(task=state)
+    print_result(result)
     pass
 
 def question_37(nr):
+    Router1_A = nr.filter(device_name="R1-CPE-BAT-A")
+    result = Router1_A.run(task=netmiko_send_config, config_commands=["interface Loopback 2","ip address 1.1.1.2 255.255.255.255"])
+    print_result(result)
+    Router2_A = nr.filter(device_name="R2-CPE-BAT-A")
+    result = Router2_A.run(task=netmiko_send_config, config_commands=["interface Loopback 2","ip address 2.2.2.3 255.255.255.255"])
+    print_result(result)
     pass
 
 def question_38(nr):
+    Router1_A = nr.filter(device_name="R1-CPE-BAT-A")
+    result = Router1_A.run(task=netmiko_save_config)
+    print_result(result)
+    Router2_A = nr.filter(device_name="R2-CPE-BAT-A")
+    result = Router2_A.run(task=netmiko_save_config)
+    print_result(result)
     pass
 
 def question_39(nr):
@@ -103,7 +187,7 @@ if __name__ == "__main__":
     #question_13(nr)
     #question_14(nr)
     #question_15(nr)
-    question_16(nr)
+    #question_16(nr)
     #question_17(nr)
     #question_18(nr)
     #question_19(nr)
@@ -118,13 +202,13 @@ if __name__ == "__main__":
     #question_29(nr)
     #question_30(nr)
 
-    #question_32(nr)
+    #○question_32(nr)
     #question_33(nr)
     #question_34(nr)
     #question_35(nr)
     #question_36(nr)
     #question_37(nr)
-    #question_38(nr)
+    question_38(nr)
     #question_39(nr)
     #question_39_d(nr)
 
